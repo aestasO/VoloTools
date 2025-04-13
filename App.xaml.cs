2212d13284e8867aeb97c0215f4d959afe2833c0
kawai <kawai@KAWAI-AK>
2025-04-13 15:03:48 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Globalization;
+using System.Threading;
+
+public partial class App : Application
+{
+    public App()
+    {
+        var cultureInfo = new CultureInfo("ja-JP"); // 日本語に設定
+        Thread.CurrentThread.CurrentCulture = cultureInfo;
+        Thread.CurrentThread.CurrentUICulture = cultureInfo;
+    }
+}
