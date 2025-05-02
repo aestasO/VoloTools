1bc0bebebb61a8e07ef899c198cff1e9b035c98f
aestasO <a@a.a>
2025-05-02 15:17:45 +0900

    アップデーター追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Net.Http;
 using System.Windows;
 using MahApps.Metro.Controls;
+using System.IO;
 
 namespace volotools
 {
@@ -26,6 +29,38 @@ namespace volotools
             InitializeComponent(); // 言語変更後に画面を再初期化
 
 
+        }
+        public static async Task RunUpdaterAsync()
+        {
+            string updateUrl = "https://yourserver.com/updates/latest.zip";
+            string tempZipPath = Path.Combine(Path.GetTempPath(), "update.zip");
+            string updaterExePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Updater.exe");
+
+            // 1. ZIP をダウンロード
+            using (HttpClient client = new HttpClient())
+            {
+                var data = await client.GetByteArrayAsync(updateUrl);
+                await File.WriteAllBytesAsync(tempZipPath, data);
+            }
+
+            // 2. Updater.exe を起動
+            if (File.Exists(updaterExePath))
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = updaterExePath,
+                    Arguments = $"\"{tempZipPath}\"",
+                    UseShellExecute = true
+                });
+
+                // 3. 自分自身を終了
+                Environment.Exit(0);
+            }
+            else
+            {
+                // エラー処理
+                Console.WriteLine("Updater.exe が見つかりません。");
+            }
         }
     }
 }
